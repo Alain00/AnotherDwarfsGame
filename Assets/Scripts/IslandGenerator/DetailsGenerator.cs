3c4b44a11b154239234a8fefef6abec8d72239b1
Alain <alain00.alvarez@gmail.com>
2020-05-21 15:26:53 -0400

    details progressing

---
+++ Assets/Scripts/IslandGenerator/DetailsGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailsGenerator
+{
+    DetailsSettings settings;
+    List<GameObject[]> detailsPlaced;
+
+    public DetailsGenerator(DetailsSettings settings)
+    {
+        this.settings = settings;
+        detailsPlaced = new List<GameObject[]>();
+    }
+
+    public int FindLayer(float height){
+        for (int i = 0; i < settings.layers.Length; i++){
+            if (height >= settings.layers[i].minHeight && height <= settings.layers[i].maxHeight){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void Generate(Vector3 point){
+        int layerIndex = FindLayer(point.y);
+        if (layerIndex == -1) return;
+        DetailsSettings.Layer layer = settings.layers[layerIndex];
+        // layer
+    }
+}
---
+++ Assets/Scripts/IslandGenerator/DetailsSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DetailsSettings
+{
+    [System.Serializable]
+    public class Layer{
+        public GameObject[] prefabs;
+        public float minHeight;
+        public float maxHeight;
+    }
+
+    public Layer[] layers;
+    
+
+}
