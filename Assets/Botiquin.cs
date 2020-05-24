9d0dd7acd11ac962c3e6508da82a2e91f8ad75fb
Alejandro <alexulloafu@gmail.com>
2020-05-23 22:45:23 -0500

    Update

--- Assets/Botiquin.cs
+++ Assets/Botiquin.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Botiquin : Item
 {
+    public float LifeAmount;
+    //public PlayerStats Player;
+    public float Ammo;
    public override void ItemAction(Quaternion Dir){
-       
+       if(Ammo <= 0){
+           gameObject.SetActive(false);
+       }
+       else Debug.Log("Vida++");
    }
 }
---
+++ Assets/Torch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : Item
+{
+    public int Ammo;
+    public GameObject AntorchaDura;
+
+    public override void ItemAction(Quaternion Dir){
+        if(Ammo > 0)
+        Instantiate(AntorchaDura , transform.position + transform.forward , Quaternion.identity);
+    }
+}
