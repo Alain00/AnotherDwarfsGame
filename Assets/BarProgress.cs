ebcd575ff0681ecbb5532fbc08f18add1e9298b7
Alejandro <alexulloafu@gmail.com>
2020-05-26 21:56:52 -0500

    LifeBar

---
+++ Assets/BarProgress.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarProgress : MonoBehaviour
+{
+    PlayerStats playerStats;
+    RectTransform BarSize;
+    void Start()
+    {
+        playerStats = GameObject.FindObjectOfType<PlayerStats>();
+        BarSize = GetComponent<RectTransform>();
+    }
+
+    
+    void Update()
+    {
+        int CurSize = Mathf.RoundToInt(playerStats.CurHealth / playerStats.MaxHealth * 100);
+        CurSize = 100 - CurSize;
+        BarSize.sizeDelta = new Vector2 (BarSize.sizeDelta.x , CurSize);
+    }
+}
