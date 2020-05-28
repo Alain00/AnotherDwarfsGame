93661cf7acca6ec2e505d9d62397cf54ac4e9596
Alejandro <alexulloafu@gmail.com>
2020-05-27 23:17:21 -0500

    UPDAte

---
+++ Assets/PlayerController/MainMenu/PlayerInMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInMenu : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetInteger("weapon" , 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
