ca871b0c4d9802761bf583c2b4e60fd31c20a9e4
Alain <alain00.alvarez@gmail.com>
2020-05-29 18:11:17 -0400

    add ammoItem

---
+++ Assets/Scripts/Items/AmmoItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoItem : MonoBehaviour
+{
+    GameObject player;
+    PlayerController controller;
+
+
+    void Start(){
+        player = GameObject.FindGameObjectWithTag("Player");
+        controller = player.GetComponent<PlayerController>();
+        List<GameObject> ammos = new List<GameObject>();
+        foreach (Gun gun in controller.Weapons){
+            if (!gun.ammoItem) continue;
+            if (gun.Comprada){
+                ammos.Add(gun.ammoItem);
+            }
+        }
+        Instantiate(ammos[Random.Range(0, ammos.Count)], transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
