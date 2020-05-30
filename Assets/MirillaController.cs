60939fb965f78b808ff918de958ddf1422020290
Alejandro <alexulloafu@gmail.com>
2020-05-30 14:53:22 -0500

    M

--- Assets/MirillaController.cs
+++ Assets/MirillaController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MirillaController : MonoBehaviour
 {
-    public Texture2D Mirilla;
+    public Transform Mirilla;
+    public PlayerController PController;
+    LayerMask layerMask;
     void Start()
     {
         Cursor.visible = false;
+        PController = GetComponent<PlayerController>();
+        layerMask = PController.layerMask;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Cursor.SetCursor(Mirilla , Vector2.zero , CursorMode.ForceSoftware);
+        Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+       
+       if(Physics.Raycast(ray.origin , ray.direction , out hit , 1000, layerMask)){
+            Vector3 hitPos = hit.point;
+            hitPos.y += 2;
+            Mirilla.position = hitPos;
+       }    
+       
+        
+        //Cursor.SetCursor(Mirilla , Vector2.zero , CursorMode.ForceSoftware);
     }
 }
