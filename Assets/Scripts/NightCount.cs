fbe09e64b36004ee6027b15bdc486f02a0cc3c47
Alain <alain00.alvarez@gmail.com>
2020-05-31 23:00:05 -0400

    add time to midnight

---
+++ Assets/Scripts/NightCount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NightCount : MonoBehaviour
+{
+    EnemiesController controller;
+    public Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = EnemiesController.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.resting){
+            timeText.text = controller.GetCurrentRestingTime().ToString();
+        }else{
+            Destroy(gameObject);
+        }
+    }
+}
