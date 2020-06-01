6fac20ea76be6d0c7e8c6382f0eb70fc779f022c
Alain <alain00.alvarez@gmail.com>
2020-05-31 21:32:47 -0400

    Ship transition update 1

---
+++ Assets/Scripts/Ship/ShipManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManager : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject brother;
+    EnemiesController controller;
+
+    void Start(){
+        Vector3 point = new Vector3(0,50,0) + transform.position;
+        Vector3 brotherPoint = point + new Vector3(0,0, 10);
+        RaycastHit hit;
+        if (Physics.Raycast(point, -transform.up, out hit)){
+            transform.position = hit.point;
+        }
+
+        if (Physics.Raycast(brotherPoint, -transform.up, out hit)){
+            brother.transform.position = hit.point;
+        }
+
+        controller = EnemiesController.main;
+        controller.OnWaveBegins += Leave;
+        controller.OnWaveCompleted += Arrive;
+    }
+
+    void Arrive(){
+        animator.SetBool("onland", true);
+    }
+
+    void Leave(){
+        animator.SetBool("onland", false);
+        HideBrother();
+    }
+
+    public void ShowBrother(){
+        brother.SetActive(true);
+    }
+
+    public void HideBrother(){
+        brother.SetActive(false);
+    }
+}
