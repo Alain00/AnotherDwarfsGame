80a96999171a8ed24597e79a5ba2a2c08a06f3f8
Alain <alain00.alvarez@gmail.com>
2020-05-31 23:32:46 -0400

    ad torch slow motion

--- Assets/PlayerController/Pinchan/FireBuff.cs
+++ Assets/PlayerController/Pinchan/FireBuff.cs
@@ -8,21 +8,27 @@ public class FireBuff : MonoBehaviour
     public LayerMask mask;
     Collider[] colliders;
 
-    public void OnTriggerStay(Collider col){
+    public void OnTriggerEnter(Collider col){
         if(col.gameObject.tag == "Enemy"){
-            AgentPathFinding enemy = col.GetComponent<AgentPathFinding>();
-            enemy.IsUnderFire = true;
+            EnemyAI enemy = col.GetComponent<EnemyAI>();
+            enemy.isUnderFire = true;
+        }
+    }
+    public void OnTriggerExit(Collider col){
+        if(col.gameObject.tag == "Enemy"){
+            EnemyAI enemy = col.GetComponent<EnemyAI>();
+            enemy.isUnderFire = false;
         }
     }
     public void Update(){
-       colliders = Physics.OverlapSphere(transform.position , radius , mask);
+       /*colliders = Physics.OverlapSphere(transform.position , radius , mask);
        if(colliders.Length > 0){
            for(int i = 0 ; i < colliders.Length ; i++){
                EnemyAI enemy = colliders[i].GetComponent<EnemyAI>();
                //enemy.IsUnderFire = true;
            }
            
-       }
+       }*/
     }
 
     public void OnDrawGizmos(){
